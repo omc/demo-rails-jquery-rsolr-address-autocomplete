6c8a2a6196947a80ce1e06c739ad4926e228287d
Nick Zadrozny <nick@beyondthepath.com>
2011-11-08 10:35:18 -0700

    wrong format for mm

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -17,7 +17,7 @@ protected
       q: params[:q].downcase,
       defType: 'dismax',
       qf: 'address_texts',
-      mm: 0.6,
+      mm: '60%',
       
       # perform highlighting on the results
       hl: true,
